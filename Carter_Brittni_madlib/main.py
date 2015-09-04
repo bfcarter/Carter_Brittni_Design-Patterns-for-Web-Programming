33e3b6220064b90ca1b2d4db897e9b456675bf34
bfcarter <bfcarter@fullsail.edu>
2015-09-04 18:46:59 -0400

    dictionary

--- Carter_Brittni_madlib/main.py
+++ Carter_Brittni_madlib/main.py
@@ -1,7 +1,11 @@
 __author__ = 'brittnicarter'
 
 def welcome():
-    print "Hi, welcome to Brittni's Pet Shop Mad Lib"
+    print "Brittni's Pet Shop Mad Lib"
+
+def name():
+    name = raw_input("Enter your name")
+    print "Hello ", name, "welcome to Brittni's Pet Shop"
 
 
 # FUNCTION Mom gives us money-----
@@ -10,9 +14,9 @@ def calcTotal(m,y):
     return total
 
 t = calcTotal(100,400)
-print "I have a total of " + str(t) + " dollars to buy a new pet"
+print "You have a total of " + str(t) + " dollars to buy a new pet"
 
-#CONDITIONAL STATEMENT budget-----
+# CONDITIONAL STATEMENT budget-----
 
 budget = 500
 
@@ -20,12 +24,18 @@ if budget > 600:
     dog = "Puppy"
     print "Our dream has come true, we can buy a new puppy"
 elif budget > 450:
-    print "Can't get a puppy but at least we can get a rescue dog"
+    print "You can't get a puppy but at least we can get a rescue dog"
 else:
     print "No dog today."
 
 
-#For Each" LOOP Type of breed ----
+# For Each" LOOP Type of breed ----
 breeds = ["Bulldog", "German Shepherd", "Labrador Retriever", "Boxer"]
 for b in breeds:
     print "One of your choices for a new dog will be " + b
+
+
+food = dict() # dictionary----------
+food = {"Blue Buffalo":"Life Protection Form","Simply Nourish":"Limited Ingredient Diet" }
+print "Don't forget to pick up " + food["Blue Buffalo"] + " dog food for your new friend"
+
