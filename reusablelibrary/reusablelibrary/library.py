4df128497e6c130d79c49266bd228fdaaa6c16ef
bfcarter <bfcarter@fullsail.edu>
2015-09-22 20:23:52 -0400

    changed to favorite car

--- reusablelibrary/reusablelibrary/library.py
+++ reusablelibrary/reusablelibrary/library.py
@@ -1,18 +1,44 @@
-class FavoriteMovies(object):
+class FavoriteCar(object):
     def __init__(self):
-        self._movie_list
+        self.__car__list = []
         #have an array to hold the movie information
         #some way to add to the array
         #generate a list of the movies
         #caluclate time span between movies
 
-    def addMovie(self,m):
-        self.movie_list.append(m)
-        print m
+    def add_car(self,m):
+        self.car__list.append(m)
+        print m.title
+
+
+    def compile_list(self):
+        output = ''
+        for season in self.__car__list:
+            output += 'Make' + car.make + '(' + str(car.year) +') ' + car.model + '<br />'
+        return output
+    def calc_time_span(self):
+        '''
+
+        calculate the time in between cars
+        '''
+        years = []
+        for movie in self.__car_list:
+            years.append(car.year)
+
+        years.sort()
+
+        num = len(years) - 1
+        span = years[num] - years[0]
+        return 'The span between your favorite car is' + str(span)
+
+    @property
+    def car__list(self):
+        return self.__car__list
 
 
 
-class MovieData(object): # Data Object
+
+class CarData(object): # Data Object
     def __init__(self):
         self.title = ''
         self._year = 0 #check for valid year
@@ -20,12 +46,12 @@ class MovieData(object): # Data Object
 
     @property
     def year(self):
-        pass
+        return self.__year
 
     @year.setter
     def year(self, y):
-        if y is > 2015:
+        if y > 2015:
            print "Error, invalid date!"
-           self._year = 2014
+           self._year = 2015
         else:
             self._year = y
--- reusablelibrary/reusablelibrary/main.py
+++ reusablelibrary/reusablelibrary/main.py
@@ -1,29 +1,79 @@
 
 import webapp2
-from library import MovieData, FavoriteMovies
+from library import CarData, FavoriteCar
 from pages import ResultsPage
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
         #page for class
         p = ResultsPage()
-        lib = FavoriteMovies
+        lib = FavoriteCar
+class Form(object):
+    def __init__(self):
+        self.title = "Welcome!"
+        self.css = " css/stylesheet.css"
+        self.head = """
 
-        #movie title
-        #year movie was made
-        #director of the film
-        md1 = MovieData()
-        md1.title = "The Princes Bride"
-        md1.year = 1989
-        md1.director = "Rob Reiner"
-        lib.addMovie(md1)
+<!DOCTYPE HTML>
+<html>
 
-        md2 = MovieData()
-        md2.title = "Dune"
-        md2.year = 1986
-        md2.director = "David Lynch"
+    <head>
+        <title>What is your favorite cAR?</title>
+        <link rel="stylesheet" href="{self.css}">
+    </head>
+    <img src= "http://brittnicarter.com/wp-content/uploads/2015/07/logo-300x300.png" width= "100" height= "115">
+    <h1>What is your Two Favorite Car?</h1>
+    <body>"""
 
 
+
+        # form
+        self.body = '''<form method="GET" action=""
+        <p> First Car </p>
+        <label>Make: </label><input type="text" name="make"/>
+        <label>Model: </label><input type="text" name="model"/>
+        <label>Year: </label><input type="text" name="year"/>
+        <input type="radio" name="transmission" value="Automatic" checked>Automatic
+        <input type="radio" name="transmission" value="Manual">Manual
+            <input type="submit" value="Submit"/>
+            <p>What color?</p>
+            <select name="colors">
+                <option value="blue">Blue</option>
+                <option value="black">Black</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="orange">Orange</option>
+                <option value="red">Red</option>
+                <option value="other">Other</option>
+            </select>
+            <input type="submit" value="Submit"/><label>Make: </label><input type="text" name="make"/>
+        <p> Second Car</p>
+        <label>Model: </label><input type="text" name="model"/>
+        <label>Year: </label><input type="text" name="year"/>
+        <input type="radio" name="transmission" value="Automatic" checked>Automatic
+        <input type="radio" name="transmission" value="Manual">Manual
+            <input type="submit" value="Submit"/>
+            <p>What color?</p>
+            <select name="colors">
+                <option value="blue">Blue</option>
+                <option value="black">Black</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="orange">Orange</option>
+                <option value="red">Red</option>
+                <option value="other">Other</option>
+            </select>
+            <input type="submit" value="Submit"/>
+            '''
+        self.close = """
+
+        </form>
+    </body>
+</html>
+    """
+
+        p.body = lib.compile_list()
         self.response.write(p.print_out())
 
 app = webapp2.WSGIApplication([
